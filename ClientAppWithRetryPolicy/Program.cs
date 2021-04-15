308ceb21272b05f4f93e9c3869ebcc97ab8bed20
Hanne-Lotta Mäenpää <hannelotta@gmail.com>
2021-04-15 19:09:08 +0300

    Remove delay between requests

--- ClientAppWithRetryPolicy/Program.cs
+++ ClientAppWithRetryPolicy/Program.cs
@@ -16,10 +16,10 @@ IAsyncPolicy<HttpResponseMessage> retryPolicy = Policy
                 Console.WriteLine();
 
                 // Uncomment the following line to test the actual rate limit response
-                return retryAfter ?? TimeSpan.FromSeconds(2);
+                //return retryAfter ?? TimeSpan.FromSeconds(2);
 
                 // Uncomment the following line to test the retry policy when the request gets rate limited
-                //return TimeSpan.FromSeconds(2);
+                return TimeSpan.FromSeconds(2);
             },
             onRetryAsync: async (response, retryCount, retryAfter, context) => {
                 Console.WriteLine("Retry count: " + retryCount);
@@ -40,8 +40,6 @@ var client = serviceProvider.GetService<ITypedClient>();
 // Call the rate limited API service in a loop
 while (true)
 {
-    await Task.Delay(TimeSpan.FromMilliseconds(500));
-
     HttpResponseMessage response = await client.TestRateLimitedService(CancellationToken.None);
 
     Console.WriteLine("HTTP response (outside retry policy): " + response.StatusCode);
